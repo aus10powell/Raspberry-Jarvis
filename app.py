056627500c4ada8e5159b8187d43261b7d8886a8
Austin Powell <phystron@gmail.com>
2021-06-25 12:36:20 -0700

    enabled taking multiple pictures sequentially. Able to display different pictures.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from picamera import PiCamera
 from time import sleep
 
 PICTURE_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "data", "pictures"
+    "static", "raspberry_pictures"
 )
 
 logger = logging.getLogger("werkzeug")
@@ -20,48 +20,54 @@ logger.addHandler(handler)
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = PICTURE_DIR
+app.config["UPLOAD_FOLDER"] = PICTURE_DIR
+
 
 def take_picture():
     """Receives click form html form and takes picture on Pi"""
     print("Camera button was clicked", file=sys.stderr)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/take_picture", methods=["POST", "GET"])
 def take_picture():
-    logging.info("page has been hit")
     if request.method == "POST":
         python_action = request.form["take_picture"]
-
-        camera = PiCamera()
-        camera.start_preview()
-        sleep(3)
         pic_fname = "image_{}.jpg".format(
             datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         )
-        camera.capture(
-            os.path.join(
-                PICTURE_DIR,
-                pic_fname
-            )
-        )
-        camera.stop_preview()
+
+        # Take a picture
+        with PiCamera() as camera:
+            sleep(1)
+            camera.capture(os.path.join(PICTURE_DIR, pic_fname))
 
         logging.info("INFO Picture Taken")
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], pic_fname)
-        return "Successfully took a picture" #render_template("show_picture.html",user_image=full_filename)#
+        picture_status="Picture Taken"
+        return render_template("take_picture.html",picture_status=picture_status,fname=pic_fname) 
     else:
         picture_status = None
         return render_template(
-            "take_picture.html", title="Jarvis Home Monitoring System", picture_status=picture_status
+            "take_picture.html",
+            title="Jarvis Home Monitoring System",
+            picture_status=picture_status,
         )
+
+@app.route("/display_pictures")
+def display_pictures():
+    image_names = os.listdir(app.config["UPLOAD_FOLDER"])
+    print(image_names)
+    full_filename = os.path.join(app.config["UPLOAD_FOLDER"],"image_20210625-120144.jpg")
+    return render_template("display_picture.html",image_names=image_names,user_image=full_filename)
 
 
 def gen(camera):
@@ -78,5 +84,4 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(debug=True)
