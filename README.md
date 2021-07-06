4526599e903b702b31899235dd0490030bb342c8
Austin Powell <phystron@gmail.com>
2021-07-06 09:22:09 -0700

    ported to blueprint for testing locally

--- README.md
+++ README.md
@@ -10,10 +10,21 @@
 ## Planned Capabilities:
 * **Air Quality**
     * **PM2.5** Store data
-    * **Air Temp**
+    * **Inside Air Temp**
 * **Visual**
     * **Count Cars**
     * **Count Pedestrians**
 
-## Setting up Web Service
+## App Structure
+```code
+|-- RaspberryJarvis
+|   |-- vision
+|   |   |-- templates/
+|   |   |   |--vision/
+|   |   |   |   |--take_picture.html
+|   |   |   |   |--display_picture.html
+
+```
+
+## Other Setting up Web Service
 * **Following tutorial:** https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import Response
+from flask import Flask, render_template, request, Response
+from vision.vision import vision_bp
 import logging
 import datetime
 import sys
 import os
 
-from picamera import PiCamera
+
 from time import sleep
 
-PICTURE_DIR = os.path.join(
-    "static", "raspberry_pictures"
-)
+PICTURE_DIR = os.path.join("static", "raspberry_pictures")
 
 logger = logging.getLogger("werkzeug")
 handler = logging.FileHandler("app.log")
@@ -21,11 +17,6 @@ logger.addHandler(handler)
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = PICTURE_DIR
-
-
-def take_picture():
-    """Receives click form html form and takes picture on Pi"""
-    print("Camera button was clicked", file=sys.stderr)
 
 
 @app.route("/")
@@ -38,50 +29,58 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/take_picture", methods=["POST", "GET"])
-def take_picture():
-    if request.method == "POST":
-        python_action = request.form["take_picture"]
-        pic_fname = "image_{}.jpg".format(
-            datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        )
+# @app.route("/take_picture", methods=["POST", "GET"])
+# def take_picture():
+#     if request.method == "POST":
+#         python_action = request.form["take_picture"]
+#         pic_fname = "image_{}.jpg".format(
+#             datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+#         )
 
-        # Take a picture
-        with PiCamera() as camera:
-            sleep(1)
-            camera.capture(os.path.join(PICTURE_DIR, pic_fname))
+#         # Take a picture
+#         with PiCamera() as camera:
+#             sleep(1)
+#             camera.capture(os.path.join(PICTURE_DIR, pic_fname))
 
-        logging.info("INFO Picture Taken")
-        picture_status="Picture Taken"
-        return render_template("take_picture.html",picture_status=picture_status,fname=pic_fname) 
-    else:
-        picture_status = None
-        return render_template(
-            "take_picture.html",
-            title="Jarvis Home Monitoring System",
-            picture_status=picture_status,
-        )
-
-@app.route("/display_pictures")
-def display_pictures():
-    image_names = os.listdir(app.config["UPLOAD_FOLDER"])
-    print(image_names)
-    full_filename = os.path.join(app.config["UPLOAD_FOLDER"],"image_20210625-120144.jpg")
-    return render_template("display_picture.html",image_names=image_names,user_image=full_filename)
+#         logging.info("INFO Picture Taken")
+#         picture_status = "Picture Taken"
+#         return render_template(
+#             "take_picture.html", picture_status=picture_status, fname=pic_fname
+#         )
+#     else:
+#         picture_status = None
+#         return render_template(
+#             "take_picture.html",
+#             title="Jarvis Home Monitoring System",
+#             picture_status=picture_status,
+#         )
 
 
-def gen(camera):
-    while True:
-        frame = PiCamera.get_frame()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+# @app.route("/display_pictures")
+# def display_pictures():
+#     image_names = os.listdir(app.config["UPLOAD_FOLDER"])
+#     print(image_names)
+#     full_filename = os.path.join(
+#         app.config["UPLOAD_FOLDER"], "image_20210625-120144.jpg"
+#     )
+#     return render_template(
+#         "display_picture.html", image_names=image_names, user_image=full_filename
+#     )
 
 
-@app.route("/video_feed")
-def video_feed():
-    return Response(
-        gen(PiCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
+# def gen(camera):
+#     while True:
+#         frame = PiCamera.get_frame()
+#         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+# @app.route("/video_feed")
+# def video_feed():
+#     return Response(
+#         gen(PiCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+#     )
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True)
+app.register_blueprint(vision_bp, url_prefix='/vision')
